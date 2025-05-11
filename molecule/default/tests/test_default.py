51b2868e8908a0e05ae2e32e982bc652b1837e57
João Brilhante <joao.brilhante@outlook.com>
2025-05-11 22:04:38 +0200

    Add a role to install Discord.

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -136,3 +136,9 @@ def test_postman_installed(host):
   result = host.run("flatpak list --app --columns=application")
   assert result.rc == 0
   assert app_id in result.stdout.splitlines()
+
+def test_discord_installed(host):
+  app_id = "com.discordapp.Discord"
+  result = host.run("flatpak list --app --columns=application")
+  assert result.rc == 0
+  assert app_id in result.stdout.splitlines()
